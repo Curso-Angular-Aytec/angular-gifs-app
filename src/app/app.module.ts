f209924f47006475b4a256c5d5393546c56e7541
Carlos Gómez Fernández <carlos.gomez33@alu.uclm.es>
2023-02-22 14:47:45 +0100

    Seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ShareModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
